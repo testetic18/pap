5845ad261d651ae6313375f268e49f2c89011bd8
testetic18 <134803199+testetic18@users.noreply.github.com>
2024-11-27 18:38:46 +0000

    Add files via upload

---
+++ pap/js/teste.js
@@ -0,0 +1,10 @@
+// Abrir o side menu
+document.getElementById("openSideMenu").onclick = function() {
+    document.getElementById("sideMenu").classList.add("open-side-menu");
+  }
+  
+  // Fechar o side menu
+  document.getElementById("closeSideMenu").onclick = function() {
+    document.getElementById("sideMenu").classList.remove("open-side-menu");
+  }
+  
